c9977d9f861e08b1af845df5f2e1380f03059f39
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-12-30 13:39:12 -0300

    Basic Menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,57 @@
 ﻿using dotNet.Models;
 
+string? opcao;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    // Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastrar cliente");
+            break;
+        case "2":
+            Console.WriteLine("Buscar cliente");
+            break;
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break; 
+        case "4":
+            Console.WriteLine("Encerrar");
+            // Environment.Exit(0);
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+}
+
+Console.WriteLine("Fim do programa");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Calculadora calc = new Calculadora();
 
 // calc.Somar(10, 5);
